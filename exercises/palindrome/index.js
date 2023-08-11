dfe07b932d056d81ee87c35d98dc29dea4ae52b7
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-08-11 13:10:46 -0400

    pallindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,39 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+// let left = 0;
+// let right = str.length - 1
+
+// while(left < str.length && right < str.length){
+//   if(left !== right){
+//     return false
+//   }else if( left === right){
+//     left++
+//     right--
+//   }
+//   return true
+
+// }
+
+// }
+
+// function palindrome(str){
+//   const reversed = str.split('').reverse().join('')
+
+//   if(reversed === str){
+//     return true
+//   } else{
+//     return false
+//   }
+// }
+
+function palindrome(str){
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  })
+
+}
+
 
 module.exports = palindrome;
