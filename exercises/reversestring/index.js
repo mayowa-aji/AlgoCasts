265c1e3662e3200f047971a96cc984c1cca757df
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-08-11 11:10:44 -0400

    reverse-string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+// return str.split('').reverse().join('')
+
+// }
+
+// function reverse(str){
+//   debugger
+//  return str.split('').reduce((rev, char) => char + rev )
+
+// }
+
+
+function reverse(str){
+  let reversed = ''
+  for(let char of str){
+    reversed = char + reversed
+  }
+  return reversed
+}
+
+
 
 module.exports = reverse;
