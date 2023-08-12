63d5183ac8790de1de978a946e8c1792fc3de2e4
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-08-11 20:41:30 -0400

    complete maxchars and fizzbuzz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {}
+  let maxCount = 0
+  let maxChar = ''
+
+  //loop through and create an object with frequency
+  for (let char of str){
+    if(charMap[char]){
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+
+  for(let char in charMap){
+    if(charMap[char] > maxCount){
+      maxCount = charMap[char]
+      maxChar = char
+    }
+  }
+
+return maxChar  
+
+  /// loop through and compare max to current value in array and return maxchar
+}
 
 module.exports = maxChar;
