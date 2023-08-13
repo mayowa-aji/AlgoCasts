6b1705f40f25935fa1fa68d7e3db43498a81d4a3
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-08-13 13:52:25 -0400

    complete anagram and chunk"

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
 
+function anagrams(stringA, stringB) {
+
+  if(cleanString(stringA) === cleanString(stringB)){
+    return true
+  }
+  return false
+
+}
+
+function cleanString(string) {
+  return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA)
+//   const bCharMap = buildCharMap(stringB)
+
+//   if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//       return false
+//   }
+
+//   for(let char in aCharMap) {
+//       if(aCharMap[char] !== bCharMap[char]){
+//           return false
+//       }
+//   }
+//   return true
+// }
+
+// function buildCharMap(string) {
+//   const charMap = {}
+//   string = string.replace(/[^\w]/g, '').toLowerCase()
+
+//   for(let char of string){
+//       if(charMap[char]) {
+//           charMap[char]++
+//       } else {
+//           charMap[char] = 1
+//       }
+//   }
+//   return charMap
+// }
 module.exports = anagrams;
